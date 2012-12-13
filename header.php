fad46e5e96c401f44db0d875d2683a2ea20f593b
brito <francisco.brito@gmail.com>
2012-12-12 17:45:09 -0800

    Updated global styles

--- header.php
+++ header.php
@@ -5,10 +5,23 @@
 <meta content="The Official website of Khaleesi, Queen of Kansas: The Brittany G"
       name=description>
 
+<!-- link rel=stylesheet href=//511.mx/all.css-->
 <style>
 /* khaleesi.la */
 @import url(http://fonts.googleapis.com/css?family=Stalemate|Berkshire+Swash|Cinzel+Decorative);
 
-body { background:#F5D5D5; color:#333; font:100%/1.6 Cinzel Decorative; text-align:center}
-h1 { font-size:999%; text-shadow:0 0 3px #faa; font-family:Stalemate }
+body { background:#ffd5cc; color:#333; font:100%/1.6 Cinzel Decorative; text-align:center }
+
+h1 { font-size:261%; text-shadow:0 0 3px #faa; font-family:Stalemate }
+	header h1 { font-size:999%; margin:0 }
+	
+a { color:#8693B2; font:130% Berkshire Swash }
+
+input { border:1px solid #E38181; font:100%/1 sans-serif; color:#444;
+  padding:.6em; border-radius:.6em; text-align:center; outline:0 }
+	input[type=submit]{ background:#333; color:#fff; font:120% Cinzel Decorative;
+	  border:0; padding:.6em 2.6em; margin:0 1em 1em }
+  
+.errorText { color:#f00; font:bold 90% sans-serif; margin:.6em 0 }  
+.fineprint { font-size:75%; color:#999 }
 </style>
