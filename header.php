0889ac284f8effed38fabf102ef7111056371bd4
Francisco Brito <francisco.brito@gmail.com>
2012-12-12 16:16:07 -0800

    styles

--- header.php
+++ header.php
@@ -5,3 +5,10 @@
 <meta content="The Official website of Khaleesi, Queen of Kansas: The Brittany G"
       name=description>
 
+<style>
+/* khaleesi.la */
+@import url(http://fonts.googleapis.com/css?family=Stalemate|Berkshire+Swash|Cinzel+Decorative);
+
+body { background:#F5D5D5; color:#333; font:100%/1.6 Cinzel Decorative; text-align:center}
+h1 { font-size:999%; text-shadow:0 0 3px #faa; font-family:Stalemate }
+</style>
--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <? $first_byte = round(microtime(true)*1e3); include 'header.php' ?>
 
-<h1>Khaleesi LA</h1>
+<h1>Khaleesi</h1>
+<h2>2013 Los Angeles, California</h2>
 
 <? include 'footer.php' ?>
 
