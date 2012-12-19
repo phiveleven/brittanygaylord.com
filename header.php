126ab2d77b1eea2f95433b8a5fb89aeed3ab901e
brito <francisco.brito@gmail.com>
2012-12-19 14:18:03 -0800

    more styles

--- header.php
+++ header.php
@@ -18,10 +18,12 @@ body { background:#ffd5cc; color:#333; font:normal 100%/1.38 Cinzel Decorative,G
 
 header h1 { font-family:Stalemate; font-size:4.23em }
 
-small { font-size:.618em; white-space:nowrap }
+small { font-size:.618em; white-space:nowrap; display:block }
 	
 a { color:#8693B2; font:bold 130% Berkshire Swash,Verdana,sans-serif }
 
+form { max-width:37em; background:rgba(255,255,255,.3); border-radius:.6em;
+	margin:1em auto; padding:0 0 1em }
 input { border:1px solid #E38181; font:100%/1 sans-serif; color:#444;
   padding:.6em; border-radius:.6em; text-align:center; outline:0 }
 form button { background:#333; color:#fff; font:120% Cinzel Decorative,Georgia,serif;
