75d2aaf5655aff6d3c2c202785a78510f6123fe8
brito <francisco.brito@gmail.com>
2012-12-19 14:07:42 -0800

    iOS meta; re-style

--- footer.php
+++ footer.php
@@ -1,6 +1,4 @@
-<footer>
-<h2>2013 Los Angeles, California</h2>
-</footer>
+<footer>2013 Los Angeles, California</footer>
 
 <script>_gaq=[['_setAccount','UA-36931826-1'],['_trackPageview']]</script>
 <script src=//www.google-analytics.com/ga.js async></script>
--- header.php
+++ header.php
@@ -2,28 +2,28 @@
 <script>_=+new Date</script>
 
 <title>Khaleesi &mdash; Los Angeles</title>
-<meta name=viewport content=initial-scale=1.0>
 <meta content="The Official website of Khaleesi, Queen of Kansas: The Brittany G"
       name=description>
+
+<? include 'ios-meta.php' ?>
 
 <!-- link rel=stylesheet href=//511.mx/all.css-->
 <style>
 /* khaleesi.la */
 @import url(http://fonts.googleapis.com/css?family=Stalemate|Berkshire+Swash|Cinzel+Decorative);
 
+* { padding:0; margin:0 }
 
-body { background:#ffd5cc; color:#333; font:normal 100%/1.6 Cinzel Decorative,Georgia,serif; text-align:center }
+body { background:#ffd5cc; color:#333; font:normal 100%/1.38 Cinzel Decorative,Georgia,serif; text-align:center }
 
-h1 { font-size:261%; font-family:Stalemate,Georgia,serif }
-	header h1 { font-size:999%; margin:0 }
+header h1 { font-family:Stalemate; font-size:4.23em }
+
+small { font-size:.618em; white-space:nowrap }
 	
 a { color:#8693B2; font:bold 130% Berkshire Swash,Verdana,sans-serif }
 
 input { border:1px solid #E38181; font:100%/1 sans-serif; color:#444;
   padding:.6em; border-radius:.6em; text-align:center; outline:0 }
-	input[type=submit]{ background:#333; color:#fff; font:120% Cinzel Decorative,Georgia,serif;
-	  border:0; padding:.6em 2.6em; margin:0 1em 1em }
-  
-.errorText { color:#f00; font:bold 90% sans-serif; margin:.6em 0 }  
-.fineprint { font-size:75%; color:#999 }
+form button { background:#333; color:#fff; font:120% Cinzel Decorative,Georgia,serif;
+  border:0; padding:0 2.6em; margin:1em; border-radius:.6em }
 </style>
