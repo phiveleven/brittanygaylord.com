338a87ff348df67e927f4db3521d04bfa1ec2fff
brito <francisco.brito@gmail.com>
2014-09-28 22:46:34 -0700

    async photo fetch

--- src/photos.js
+++ src/photos.js
@@ -1,5 +1,7 @@
-$.getScript('src/jquery.flickr.js', function(){
-    $('#photos').flickr({ link_to_size:'m', thumbnail_size:'m' })
-        .photosSearch({ user_id:'91992600@N03' });
+$(function(){
+    $.getScript('src/jquery.flickr.js', function(){
+        $('#photos').flickr({ link_to_size:'m', thumbnail_size:'m' })
+            .photosSearch({ user_id:'91992600@N03' });
+    });
 });
 
