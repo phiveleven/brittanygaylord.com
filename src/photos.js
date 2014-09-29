aea16b8d3fe786458a9799234b4d78494e7a0ba1
brito <francisco.brito@gmail.com>
2014-09-28 22:44:52 -0700

    photo sizes

--- src/photos.js
+++ src/photos.js
@@ -1,5 +1,5 @@
 $.getScript('src/jquery.flickr.js', function(){
-    $('#photos').flickr()
+    $('#photos').flickr({ link_to_size:'m', thumbnail_size:'m' })
         .photosSearch({ user_id:'91992600@N03' });
 });
 
